a378feb37ac4479b382431e86682f26b2f095d79
Cerlancism <scheduledx@gmail.com>
2020-09-21 08:41:47 +0800

    Update bit_printer.c

--- utils/bit_printer.c
+++ utils/bit_printer.c
@@ -6,7 +6,8 @@ void printIntAsBinary(int input, int length)
 {
     do
     {
-        putchar(((input >> (length - 1)) & 1) + 48); // Prints the bit at position from the most to least significant bit
+        // https://stackoverflow.com/questions/21255038/what-does-putchar0-num-do
+        putchar('0' + ((input >> (length - 1)) & 1)); // Prints the bit at position from the most to least significant bit
     } while (--length);
     putchar('\n');
 }
