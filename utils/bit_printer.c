4486e1d598d5271c982742ac72b2842695c482bf
Cerlancism <scheduledx@gmail.com>
2020-09-21 08:32:39 +0800

    Macro bit rotation

--- utils/bit_printer.c
+++ utils/bit_printer.c
@@ -6,7 +6,7 @@ void printIntAsBinary(int input, int length)
 {
     do
     {
-        printf("%d", ((input >> (length - 1)) & 1)); // Prints the bit at position from the most to least significant  bit
+        putchar(((input >> (length - 1)) & 1) + 48); // Prints the bit at position from the most to least significant bit
     } while (--length);
-    puts("");
+    putchar('\n');
 }
