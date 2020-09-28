b138f34a28ffa4356116f5a8a57e6525f12c6b48
catkane-doodles <tanpeikai@hotmail.com>
2020-09-28 20:22:28 +0800

    messed with chalk, but self sabotage instead

--- pythonFiles/dataGen.py
+++ pythonFiles/dataGen.py
@@ -2,14 +2,51 @@ from sense_hat import SenseHat
 import math
 import time
 import csv
+import atexit
+
+
+def toMessWithChalk(angle):
+    coors = list()
+
+    def drawLine(x1, x2, y1, y2):
+        m = (y2 - y1) / (x2 - x1)
+        return [[x, round(m*x)] for x in range(-4, 4)]
+
+    def pixelise(coors):
+        results = []
+        blank = [0, 0, 0]
+        red = [100, 0, 0]
+        for layerOne in coors:
+            grid = [blank for p in range(64)]
+            for layerTwo in layerOne:
+                grid[32 + layerTwo[0] + (layerTwo[1] * 8)] = red
+            results.append(grid)
+
+        return results
+
+    for a in range(0, angle, 5):
+        r = a * math.pi/180
+        x = math.cos(r)
+        y = math.sin(r)
+
+        coors.append(drawLine(x, -x, y, -y) + drawLine(x, -x, -y, y))
+
+    return pixelise(coors)
+
+
+crosses = toMessWithChalk(30)
+xc = 0
+
 
 red = [100, 0, 0]
 green = [0, 100, 0]
+blank = [0, 0, 0]
 
 acts = ["walking", "running", "jumping",
         "stationary", "e"]  # Expected activities
 
 sense = SenseHat()
+atexit.register(sense.clear)
 
 while True:
     run = False
@@ -28,7 +65,7 @@ while True:
                 sense.show_letter(acts[c][0], green)
 
     # Creating csv file for logging of data
-    file = "../dataFiles/" + acts[c] + "/" + acts[c] + \
+    file = "./dataFiles/" + acts[c] + "/" + acts[c] + \
         "-" + time.strftime("%d-%m-%Y-%H:%M:%S") + ".csv"
     f = open(file, "w", newline="")
     dataCSV = csv.writer(f)
@@ -37,6 +74,8 @@ while True:
     dataCSV.writerow(labels)
 
     while run:
+        sense.set_pixels(crosses[xc])
+        xc = (xc + 1) % len(crosses)
         data = sense.get_accelerometer_raw()
         x = data["x"]
         y = data["y"]
