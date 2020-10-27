dc56727af9ede40b18458d74ab1cbe5ca8b90fb9
Cerlancism <scheduledx@gmail.com>
2020-10-27 08:38:24 +0800

    Update array2d_test.c

--- test/array2d_test.c
+++ test/array2d_test.c
@@ -9,7 +9,7 @@ ArrayList getRow()
 {
     ArrayList output = arraylist_new(double, ROW_LENGTH);
     // Generate a row of <ROW_LENGTH> values
-    for (double i = 0; i < ROW_LENGTH; i+=1.0)
+    for (double i = 0; i < ROW_LENGTH; i += 1.0)
     {
         arraylist_push(output, &i);
     }
@@ -19,15 +19,16 @@ ArrayList getRow()
 void getDataset()
 {
     ArrayList dataset = arraylist_new(double *, 0);
+    ArrayList datasetDisposable = arraylist_new(ArrayList, 0);
     // Generate <ROWS> amounts of rows
     for (size_t i = 0; i < ROWS; i++)
     {
         ArrayList row = getRow();
         arraylist_push(dataset, &row->_array);
+        arraylist_push(datasetDisposable, &row);
     }
 
-    double** double2d = (double**)dataset->_array;
-
+    double **double2d = (double **)dataset->_array;
     for (size_t i = 0; i < dataset->length; i++)
     {
         printf("Row[%zu]: ", i);
@@ -43,9 +44,11 @@ void getDataset()
     // Freeing up memory
     for (size_t i = 0; i < dataset->length; i++)
     {
-        ArrayList item = *(ArrayList*)arraylist_elementAt(dataset, i);
+        ArrayList item = *(ArrayList *)arraylist_elementAt(datasetDisposable, i);        
         arraylist_destroy(item);
     }
+    arraylist_destroy(datasetDisposable);
+    dataset->_array = NULL;
     arraylist_destroy(dataset);
 }
 
