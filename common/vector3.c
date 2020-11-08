7097a5b97a12ef23f2c53fea274507f75dd9ad99
Chen Yu <scheduledx@gmail.com>
2020-11-08 22:52:40 +0800

    Fix  duplicate ‘const’ declaration specifier warning and use switch statement for state drawing

--- common/vector3.c
+++ common/vector3.c
@@ -18,38 +18,38 @@ const Vector3 vector3_zero = {0, 0, 0};
 #define squareMagnitude(vector) \
     (vector->x * vector->x + vector->y * vector->y + vector->z * vector->z)
 
-Vector3 vector3_multiply(const Vector3 const *vector, double scaler)
+Vector3 vector3_multiply(const Vector3 *vector, double scaler)
 {
     scaling(vector, scaler, *);
 }
 
-Vector3 vector3_divide(const Vector3 const *vector, double divider)
+Vector3 vector3_divide(const Vector3 *vector, double divider)
 {
     scaling(vector, divider, /);
 }
 
-double vector3_sqrMagnitude(const Vector3 const *vector)
+double vector3_sqrMagnitude(const Vector3 *vector)
 {
     return squareMagnitude(vector);
 }
 
-double vector3_magnitude(const Vector3 const *vector)
+double vector3_magnitude(const Vector3 *vector)
 {
     return sqrt(squareMagnitude(vector));
 }
 
-Vector3 vector3_normalise(const Vector3 const *vector)
+Vector3 vector3_normalise(const Vector3 *vector)
 {
     double magnitude = vector3_magnitude(vector);
     return magnitude > kEpsilon ? vector3_divide(vector, magnitude) : vector3_zero;
 }
 
-double vector3_dot(const Vector3 const *a, const Vector3 const *b)
+double vector3_dot(const Vector3 *a, const Vector3 *b)
 {
     return a->x * b->x + a->y * b->y + a->z * b->z;
 }
 
-double vector3_angle(const Vector3 const *from, const Vector3 const *to)
+double vector3_angle(const Vector3 *from, const Vector3 *to)
 {
     double denominator = sqrt(vector3_sqrMagnitude(from) * vector3_sqrMagnitude(to));
     if (denominator < kEpsilonNormalSqrt)
@@ -61,7 +61,7 @@ double vector3_angle(const Vector3 const *from, const Vector3 const *to)
     return acos(dot) * 180.0 / 3.14159265358979323846;
 }
 
-double vector3_distance(const Vector3 const *a, const Vector3 const *b)
+double vector3_distance(const Vector3 *a, const Vector3 *b)
 {
     double x = a->x - b->x;
     double y = a->y - b->y;
@@ -69,7 +69,7 @@ double vector3_distance(const Vector3 const *a, const Vector3 const *b)
     return sqrt(x * x + y * y + z * z);
 }
 
-void vector3_print(const Vector3 const *vector)
+void vector3_print(const Vector3 *vector)
 {
     printf("{ x: %lf, y: %lf, z: %lf }\n", vector->x, vector->y, vector->z);
 }
--- common/vector3.h
+++ common/vector3.h
@@ -10,17 +10,17 @@ typedef struct vector3
 
 extern const Vector3 vector3_zero;
 
-Vector3 vector3_multiply(const Vector3 const *vector, double scaler);
-Vector3 vector3_divide(const Vector3 const *vector, double divider);
+Vector3 vector3_multiply(const Vector3 *vector, double scaler);
+Vector3 vector3_divide(const Vector3 *vector, double divider);
 
-double vector3_magnitude(const Vector3 const *vector);
-double vector3_sqrMagnitude(const Vector3 const *vector);
-Vector3 vector3_normalise(const Vector3 const *vector);
+double vector3_magnitude(const Vector3 *vector);
+double vector3_sqrMagnitude(const Vector3 *vector);
+Vector3 vector3_normalise(const Vector3 *vector);
 
-double vector3_dot(const Vector3 const *a, const Vector3 const *b);
-double vector3_angle(const Vector3 const *from, const Vector3 const *to);
-double vector3_distance(const Vector3 const *a, const Vector3 const *b);
+double vector3_dot(const Vector3 *a, const Vector3 *b);
+double vector3_angle(const Vector3 *from, const Vector3 *to);
+double vector3_distance(const Vector3 *a, const Vector3 *b);
 
-void vector3_print(const Vector3 const *vector);
+void vector3_print(const Vector3 *vector);
 
 #endif
